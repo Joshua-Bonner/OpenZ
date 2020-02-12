08013dd130ec1be36cf9a0a79d44fee9ec4fe502
Joshua-Bonner <joshua.bonner@outlook.com>
2020-02-12 17:00:12 -0500

    Modified musicPlayer

--- MusicPlayer/src/musicPlayer.java
+++ MusicPlayer/src/musicPlayer.java
@@ -5,4 +5,54 @@ public class musicPlayer {
     private ArrayList<album> albums;
     private int currentAlbumIndex;
     private int librarySize;
+    private boolean isPaused;
+    private int pausedSongTime;
+
+    public musicPlayer(){
+        albumIndex = 0;
+        albums = new ArrayList<>();
+        currentAlbumIndex = 0;
+        librarySize = 0;
+        isPaused = false;
+        pausedSongTime = 0;
+    }
+
+    public album getAlbum(int albumIndex){
+        return albums.get(albumIndex);
+    }
+
+    public int nextAlbum(){
+        currentAlbumIndex++;
+        if (currentAlbumIndex >= albums.size()) currentAlbumIndex = 0;
+        return currentAlbumIndex;
+    }
+
+    public void pause(){
+        //TODO: Implement this method
+        isPaused = true;
+    }
+
+    public void play(){
+        //TODO: Implement this method
+    }
+
+    public void nextSong(){
+        //TODO: Implement this method
+    }
+
+    public void previousSong(){
+        //TODO: Implement this method
+    }
+
+    public void selectAlbum(int selection){
+        //TODO: Implement this method
+    }
+
+    public void syncUSB(){
+        //TODO: Implement this method
+    }
+
+    public void drawVisualizer(){
+        //TODO: Implement this method
+    }
 }
