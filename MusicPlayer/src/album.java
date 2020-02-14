05d850e6fca82817f5f3c34bd17843824432469a
sagethewizard <jag6248@psu.edu>
2020-02-14 01:49:50 -0500

    Added TODO for albumName in album class

--- MusicPlayer/src/album.java
+++ MusicPlayer/src/album.java
@@ -1,28 +1,17 @@
-import javazoom.spi.mpeg.sampled.file.MpegAudioFileFormat;
-import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader;
 import org.farng.mp3.MP3File;
 import org.farng.mp3.TagException;
 import org.farng.mp3.id3.AbstractID3v2;
 import org.farng.mp3.id3.ID3v1;
-import org.jaudiotagger.audio.AudioFile;
-import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
-import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.File;
-import java.util.Arrays;
-import java.util.Map;
 
 public class album {
     private String albumArtist;
     private String albumCoverLocation;
     private String albumLocation;
+    private String albumName; // TODO: Modify Constructor to pull album name tags for this var
     private int currentSongIndex;
     private ArrayList<song> songs;
 
@@ -87,18 +76,16 @@ public class album {
         albumLocation = albumLoc;
         currentSongIndex = 0;
         songs = new ArrayList<>();
+
+        boolean foundAlbumName;
+
         System.out.println("In Album Constructor");
 
-        //#TODO Here: Fill AList w/ songs using albumLoc
-        // https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
-        // May be useful.. will need to type case "File" MP3 related object to obtain tags
         File folder = new File(albumLoc);
         String[] filesInDir = folder.list();
         String[] fileTokens;
         File tempFile;
-        AudioFile base;
         MP3File tempMp3;
-        Map properties;
         ID3v1 tempIDv1;
         AbstractID3v2 tempIDv2;
         int tagVersion = 0;
@@ -110,20 +97,21 @@ public class album {
             if (fileTokens[fileTokens.length - 1].toUpperCase().equals("MP3")) {
                 try {
                     tempFile = new File(albumLoc + s);
-                    // issues getting time w/ VVVVVVVVVV tried mutiple packages
-                    //base = AudioFileIO.read(new File(albumLoc + s));
                     tempMp3 = new MP3File(tempFile);
 
                     if (tempMp3.hasID3v1Tag()) {
                         tempIDv1 = tempMp3.getID3v1Tag();
                         songs.add(new song(1, albumLoc + s, tempIDv1.getSongTitle(), 1));
+                        // TODO: Pull tag here in if block for album name using foundAlbumName bool, switch to true when found
                         tagVersion = 1;
                     } else if (tempMp3.hasID3v2Tag()) {
                         tempIDv2 = tempMp3.getID3v2Tag();
                         songs.add(new song(1, albumLoc + s, tempIDv2.getSongTitle(), 2));
+                        // TODO: Pull tag here in if block for album name using foundAlbumName bool, switch to true when found
                         tagVersion = 2;
                     } else {
                         songs.add(new song(1, albumLoc + s, "UNKNOWN", 0));
+                        albumName = "UNKNOWN";
                         tagVersion = 0;
                     }
 
@@ -184,5 +172,7 @@ public class album {
     public String getLocation() {
         return albumLocation;
     }
+
+    public String getAlbumName() {return albumName; }
 
 }
