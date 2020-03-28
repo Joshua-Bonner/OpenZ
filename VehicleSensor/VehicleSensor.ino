76c5ddf1f78f14509dacc7f2de98a25a4039d62b
Joshua-Bonner <joshua.bonner@outlook.com>
2020-03-28 12:36:53 -0400

    Added error detection

--- VehicleSensor/VehicleSensor.ino
+++ VehicleSensor/VehicleSensor.ino
@@ -5,8 +5,11 @@
 #define ECHO_PIN2 9
 #define TRIG_PIN3 8
 #define ECHO_PIN3 7
-#define TRIG_PIN4 6
-#define ECHO_PIN4 5
+#define TRIG_PIN4 4
+#define ECHO_PIN4 2
+#define BLUE 6
+#define GREEN 5
+#define RED 3
 
 SR04 sensor1 = SR04(ECHO_PIN,TRIG_PIN);
 SR04 sensor2 = SR04(ECHO_PIN2,TRIG_PIN2);
@@ -21,14 +24,20 @@ long distance3;
 long distance4;
 long minDist;
 float buzzTime;
+int on = 255;
+int off = 0;
 
 void setup() {
    Serial.begin(9600);
    pinMode(buzz, OUTPUT);
+   pinMode(RED, OUTPUT);
+   pinMode(GREEN, OUTPUT);
+   pinMode(BLUE, OUTPUT);
 }
 
 void loop() {
-   delay(50);
+   analogWrite(GREEN, on);
+   analogWrite(BLUE, off);
    minDist = 1000;
    distance1 = sensor1.Distance();
    distance2 = sensor2.Distance();
@@ -39,14 +48,22 @@ void loop() {
    if (distance2 != 0 && minDist >= distance2) minDist = distance2;
    if (distance3 != 0 && minDist >= distance3) minDist = distance3;
    if (distance4 != 0 && minDist >= distance4) minDist = distance4;
+   if (distance1 == 0 || distance2 == 0 || distance3 == 0 || distance4 == 0){
+    analogWrite(BLUE, on);
+    tone(buzz, 100);
+    delay(500);
+    noTone(buzz);
+   }
 
    buzzTime = 150 / 50 + minDist;
-   
-   if (minDist <= 100){
+
+   if (minDist <= 75){
+    analogWrite(GREEN, off);
     tone(buzz, 2000);
+    analogWrite(RED, on);
     delay(buzzTime*5);
+    analogWrite(RED, off);
     noTone(buzz);
-    delay(buzzTime*5);
    }
    
    // Prints out distance via the serial monitor found under the tools tab
