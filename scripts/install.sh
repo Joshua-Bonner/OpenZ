f00ecf9d1cdd71beffe481fa64e9b28761b0c943
Richard Bowser <techdude154@gmail.com>
2020-03-29 19:05:25 -0400

    added install script

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/pi
+git clone https://github.com/Joshua-Bonner/OpenZ.git
+cd /home/pi/OpenZ
+sudo apt-get install python2.7 openjdk-8-jdk
+pip install obd
+pip install pillow
+pip install pydub
+sudo echo "python /home/pi/OpenZ/Lane_Departure_Detection/gafbm.py &" >> /etc/rc.local
+sudo echo "python /home/pi/OpenZ/OBD/OBDServer/OBDServer.py &" >> /etc/rc.local 
+sudo echo "java -jar  /home/pi/OpenZ/OBD/OBDServer/OBDServer.py &" >> /etc/rc.local #TODO add path of JAR File
